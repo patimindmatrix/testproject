083bc278b527eec8d5cae6c84d73b1a17de97619
Pati <pati@mindmatrix.vn>
2023-11-29 09:37:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hôm nay asao cao CDCS
         </p>
         <a
           className="App-link"
